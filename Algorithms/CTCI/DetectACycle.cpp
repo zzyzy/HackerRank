6008e19897c966fa1d51d9394d5a214690c97489
zzyzy <leezhenzhi@gmail.com>
2017-01-27 00:21:38 +1100

    Completed CTCI DetectACycle challenge

---
+++ Algorithms/CTCI/DetectACycle.cpp
@@ -0,0 +1,80 @@
+/*
+ * HackerRank CTCI Challenges
+ * Linked Lists: Detect a Cycle
+ * https://www.hackerrank.com/challenges/ctci-linked-list-cycle
+ *
+ * Written by Zhen Zhi Lee in C++11
+ * Last modified 26/01/2017
+ */
+
+#include <iostream>
+#include <unordered_set>
+
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node *next;
+};
+
+Node *addNode(Node *node, int data) {
+    if (!node) {
+        node = new Node();
+        node->data = data;
+        node->next = nullptr;
+        return node;
+    } else {
+        node->next = new Node();
+        node->next->data = data;
+        node->next->next = nullptr;
+        return node->next;
+    }
+}
+
+void clearList(Node *head) {
+    if (!head) return;
+    unordered_set<Node *> references;
+
+    Node *current = head;
+    Node *next;
+    while (current != nullptr) {
+        if (references.find(current) != references.end()) {
+            break;
+        } else {
+            references.insert(current);
+        }
+        next = current->next;
+        delete current;
+        current = next;
+    }
+}
+
+bool hasCycle(Node *head) {
+    if (!head) return false;
+    unordered_set<Node *> references;
+
+    Node *ptr = head;
+    while (ptr != nullptr) {
+        if (references.find(ptr) != references.end()) {
+            return true;
+        } else {
+            references.insert(ptr);
+        }
+        ptr = ptr->next;
+    }
+
+    return false;
+}
+
+int main() {
+    auto head = addNode(nullptr, 1);
+    auto node2 = addNode(head, 2);
+    auto node3 = addNode(node2, 3);
+    node3->next = node2;
+
+    cout << hasCycle(head) << endl;
+
+    clearList(head);
+
+    return 0;
+}
